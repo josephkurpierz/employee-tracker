31e76520b2c8fb31045b3d0bae1ec3b727c9ad91
Joseph Kurpierz <josephkurpierz@gmail.com>
2022-07-24 19:51:33 -0500

    update employee role

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const db = require("./db/connection");
 const cTable = require("console.table");
+const { promise } = require("./db/connection");
 
 const promptMenu = () => {
   return inquirer
@@ -59,7 +60,7 @@ const selectDepartments = () => {
 
 const selectRoles = () => {
   db.query(
-    `SELECT roles.title, roles.salary, departments.name AS department 
+    `SELECT roles.id, roles.title, roles.salary, departments.name AS department 
     FROM roles 
     LEFT JOIN departments
     ON roles.department_id=departments.id`,
@@ -83,11 +84,13 @@ const selectEmployees = () => {
      LEFT JOIN departments d
      ON r.department_id = d.id
      LEFT JOIN employees m
-     ON e.manager_id = m.id`, (err, results) => {
-      console.log("error",err);
-    console.table(results);
-    promptMenu();
-  });
+     ON e.manager_id = m.id`,
+    (err, results) => {
+      console.log("error", err);
+      console.table(results);
+      promptMenu();
+    }
+  );
 };
 
 const addDepartment = () => {
@@ -123,52 +126,231 @@ const addDepartment = () => {
 };
 
 const addRole = () => {
+  db.query(`SELECT * FROM departments`, function (err, res) {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "title",
+          message: "What is title of the new role?",
+          validate: (title) => {
+            if (title) {
+              return true;
+            } else {
+              console.log("please enther the title of the new role");
+              return false;
+            }
+          },
+        },
+        {
+          type: "input",
+          name: "salary",
+          message: "what is the salary for the new role?",
+          validate: (salary) => {
+            if (salary) {
+              return true;
+            } else {
+              console.log("enter the salary for the role");
+              return false;
+            }
+          },
+        },
+        {
+          type: "list",
+          name: "department",
+          message: "What department is this new role for?",
+          //makes new array of department names for choices, including any recently created
+          choices: () => {
+            const departmentNameArr = [];
+            for (let i = 0; i < res.length; i++) {
+              departmentNameArr.push(res[i].name);
+            }
+            return departmentNameArr;
+          },
+        },
+      ])
+      .then((answer) => {
+        let departmentId;
+        for (let index = 0; index < res.length; index++) {
+          if (res[index].name == answer.department) {
+            departmentId = res[index].id;
+          }
+        }
+
+        db.query(
+          `INSERT INTO roles SET ?`,
+          {
+            title: answer.title,
+            salary: answer.salary,
+            department_id: departmentId,
+          },
+          (err, res) => {
+            if (err) throw err;
+            console.log("role has been added");
+            promptMenu();
+          }
+        );
+      });
+  });
+};
+
+const addEmployee = () => {
+  db.query(`SELECT * FROM roles`, (err, res) => {
+    if (err) throw err;
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          name: "first_name",
+          message: "What is the employee first name?",
+        },
+        {
+          type: "input",
+          name: "last_name",
+          message: "What is the employee last name?",
+        },
+        {
+          type: "list",
+          name: "title",
+          message: "what is the employee's role?",
+          choices: () => {
+            let roleArr = [];
+            for (let i = 0; i < res.length; i++) {
+              roleArr.push(res[i].title);
+            }
+            return roleArr;
+          },
+        },
+        {
+          type: "input",
+          name: "manager",
+          message: "enter manager's id",
+        },
+      ])
+      .then((answer) => {
+        let roleId;
+        for (let index = 0; index < res.length; index++) {
+          if (res[index].title == answer.title) {
+            roleId = res[index].id;
+          }
+        }
+        db.query(
+          `INSERT INTO employees SET ?`,
+          {
+            first_name: answer.first_name,
+            last_name: answer.last_name,
+            role_id: roleId,
+            manager_id: answer.manager,
+          },
+          (err) => {
+            if (err) throw err;
+            promptMenu();
+          }
+        );
+      });
+  });
+};
+
+// Concatenates employee first and last names
+function getEmployeeFirstAndLast() {
+  return new Promise((resolve) => {
+    db.query(`SELECT first_name, last_name FROM employees`, (err, rows) => {
+      if (err) throw err;
+      let employee = [];
+      for (i = 0; i < rows.length; i++) {
+        employee.push({
+          id: i,
+          name: rows[i].first_name + " " + rows[i].last_name,
+        });
+      }
+      resolve(employee);
+    });
+  });
+}
+
+// returns an array of the concatenation of employee's first and last names asynchronously
+async function asyncEmployee() {
+  const result = await getEmployeeFirstAndLast();
+  return result;
+}
+
+//get array of roles
+function getRoleOptions() {
+  return new Promise((resolve) => {
+    db.query(`SELECT title FROM roles`, (err, rows) => {
+      if (err) throw err;
+      let rolesArr = [];
+      for (i = 0; i < rows.length; i++) {
+        rolesArr.push(rows[i].title);
+      }
+      resolve(rolesArr);
+    });
+  });
+}
+
+// asynchronously returns an array of available roles
+async function asyncRole() {
+  const result = await getRoleOptions();
+  return result;
+}
+
+const updateEmployee = () => {
   inquirer
     .prompt([
       {
-        type: "input",
-        name: "title",
-        message: "What is title of the new role?",
-        validate: (title) => {
-          if (title) {
-            return true;
-          } else {
-            console.log("please enther the title of the new role");
-            return false;
-          }
-        },
-      },
-      {
-        type: "input",
-        name: "salary",
-        message: "what is the salary for the new role?",
-        validate: (salary) => {
-          if (salary) {
-            return true;
-          } else {
-            console.log("enter the salary for the role");
-            return false;
-          }
-        },
-      },
-      {
         type: "list",
-        name: "department",
-        message: "What department is this new role for?",
-        choices: departments,
+        name: "employee",
+        id: "id",
+        message: "choose the employee",
+        choices: () => {
+          return asyncEmployee();
+        },
       },
     ])
-    .then(title, salary, (department) => {
+    .then((name) => {
       db.query(
-        `INSERT INTO roles SET title = ?, salary = ?, department_id = ?`,
-        [title, salary, department]
+        `SELECT * FROM employees WHERE CONCAT(first_name," ",last_name) = ?`,
+        name.employee,
+        (err, res) => {
+          if (err) throw err;
+          let employeeId = res[0].id;
+          inquirer
+            .prompt([
+              {
+                type: "list",
+                name: "title",
+                message: "what is the employee's new role",
+                choices: () => {
+                  return asyncRole();
+                },
+              },
+            ])
+            .then((data) => {
+              db.query(
+                `SELECT * FROM roles WHERE title =?`,
+                data.title,
+                (err, res) => {
+                  if (err) throw err;
+                  let roleId = res[0].id;
+
+                  db.query(
+                    `UPDATE employees SET role_id = ? WHERE id = ?`,
+                    [roleId, employeeId],
+                    (err, res) => {
+                      if (err) throw err;
+                      console.log("employee role updated");
+                      promptMenu();
+                    }
+                  );
+                }
+              );
+            });
+        }
       );
-      promptMenu();
     });
 };
-
-const addEmployee = () => {};
-
-const updateEmployee = () => {};
 
 promptMenu();
